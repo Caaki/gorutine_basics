27d274be485222ac9429ec1378fba23af8d80480
Caaki <nikola.lazin.4384@metropolitan.ac.rs>
2024-06-23 13:21:00 +0100

    First encounter with channels

---
+++ channels.go
@@ -0,0 +1,25 @@
+package main
+
+import (
+	"fmt"
+	"math/rand"
+)
+
+// Za ovaj primer je bitno primetiti i zapamtiti da se nit zaustavlja posto ceka podatke od kanala c
+// Zato nije potrebno implementirati wait grupe
+func getRandomNumber(c chan int32) {
+	theRandomNumber := rand.Int31n(1000)
+	fmt.Printf("The random number is: %d\n", theRandomNumber)
+
+	c <- theRandomNumber
+
+}
+
+func main() {
+	c := make(chan int32)
+	go getRandomNumber(c)
+
+	output := <-c
+	fmt.Println(output)
+
+}
