e08adca65ab34d6de4e43d9948bc0d619ef8573b
Caaki <nikola.lazin.4384@metropolitan.ac.rs>
2024-07-02 22:56:02 +0100

    test method added, not finished, need to read more stuff from the documentation

--- buffered_chanels.go
+++ buffered_chanels.go
@@ -2,12 +2,49 @@ package main
 
 import (
 	"fmt"
+	"math/rand"
 	"time"
 )
 
 // Ovaj primer pokazuje kako mozemo da postavimo kanal i proveravamo da li ima poruku i kada se pojavi
 // poruka odma je ispisemo
 func main() {
+	//channel := make(chan int)
+	//go metodaKojaKoristiRange(channel)
+	//
+
+	go metodaSaCasom()
+	time.Sleep(time.Second * 20)
+
+}
+
+func randomNum(channel chan int, brojPoruka int) {
+
+	for i := 0; i < brojPoruka; i++ {
+		channel <- rand.Intn(5)
+	}
+
+	close(channel)
+}
+
+func metodaKojaKoristiRange(channel chan int) {
+
+	go randomNum(channel, 5)
+	//for {
+	//	number, ok := <-channel
+	//	if !ok {
+	//		break
+	//	}
+	//	fmt.Println(number)
+	//}
+
+	for _ = range channel {
+		fmt.Println(<-channel)
+	}
+
+}
+
+func metodaSaCasom() {
 
 	kanal := make(chan string, 2)
 	kanal <- "Prva poruka"
@@ -41,5 +78,4 @@ func main() {
 	}
 
 	fmt.Println("Program ended, server was idle for too long")
-
 }
--- go.mod
+++ go.mod
@@ -1,1 +1,3 @@
 module github/Caaki/gorutines_basics
+
+go 1.22
