652dc4dcbfe6a69b12fbc573427e73791c9dd362
Caaki <nikola.lazin.4384@metropolitan.ac.rs>
2024-08-07 10:23:05 +0200

    Added listening for messages from different channels

--- usingRangeForChannels.go
+++ usingRangeForChannels.go
@@ -7,24 +7,39 @@ import (
 
 func main() {
 
-	channel := make(chan int)
-	go getNumbers(channel)
-	for {
-		if message, ok := <-channel; ok {
-			fmt.Println(message)
-		}
-		break
-	}
+	channel := make(chan string)
+	channel2 := make(chan string)
+	go getNumbers(channel, "1")
+	go getNumbers(channel2, "2")
 
+	for {
+		select {
+		case message, ok := <-channel:
+			if ok {
+				fmt.Println(message)
+			}
+		case message, ok := <-channel2:
+			if ok {
+				fmt.Println(message)
+			}
+		default:
+
+		}
+		//	if message, ok := <-channel; ok {
+		//		fmt.Println(message)
+		//	}
+		//	break
+		//}
+	}
 }
 
-func getNumbers(channel chan int) {
+func getNumbers(channel chan string, message string) {
 
 	numberOfRound := rand.Intn(5) + 1
 	fmt.Println(numberOfRound)
 
 	for range numberOfRound {
-		channel <- rand.Intn(10)
+		channel <- fmt.Sprintf(" Message from channel %s [%d]", message, rand.Intn(10))
 	}
 	close(channel)
 }
